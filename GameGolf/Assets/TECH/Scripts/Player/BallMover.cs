2f360dbef0bf3e4698c91d97e75a2be23f2aa41b
ElieBouillard <75082918+ElieBouillard@users.noreply.github.com>
2021-12-08 10:02:18 +0100

    [ADD] - ball mover test

--- GameGolf/Assets/TECH/Scripts/Player/BallMover.cs
+++ GameGolf/Assets/TECH/Scripts/Player/BallMover.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class BallMover : MonoBehaviour
 {
-    
+    [SerializeField] private LayerMask _floorMask = new LayerMask();
+    [SerializeField] private GameObject _directionOnFloor = null;
+
+    private Vector3 _currDirection = new Vector3();
+
+    Vector3 mouseHit = new Vector3();
+
+    private void Update()
+    {
+        mouseHit = GetMouseClickWorldPos();
+
+        _currDirection = (mouseHit - transform.position).normalized;
+
+        _directionOnFloor.transform.forward = _currDirection;
+       
+    }
+
+    private Vector3 GetMouseClickWorldPos()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _floorMask);
+        return hit.point;
+    }
 }
